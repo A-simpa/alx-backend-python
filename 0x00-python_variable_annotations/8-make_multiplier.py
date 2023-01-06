06ec7818737728a33710995888fc6fc5400c77d4
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-06 23:13:47 +0100

    new: added 8-make_multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""file for a type-annotated function make_multiplier
+that takes a float multiplier as argument and
+returns a function
+that multiplies a float by multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float and returns a function that multiplier a float"""
+    x: Callable[[float], float] = lambda f: multiplier * f
