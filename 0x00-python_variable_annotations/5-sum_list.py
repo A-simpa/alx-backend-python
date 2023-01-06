95446d66f050431466409bc3bf856a6e92560582
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-06 22:28:54 +0100

    refactor: using typing instead

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,7 +3,9 @@
 which takes a list input_list of floats as argument
 and returns their sum as a float."""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """sums a list of float"""
     return sum(input_list)
