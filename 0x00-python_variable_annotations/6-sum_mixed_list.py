fedef7208e6de7d26c686197111d879e082b8f8d
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-06 22:38:05 +0100

    new: added 6-sum_mixed_list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,4 +8,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """sums a list of float"""
-    return sum(input_list)
+    return sum(float(input_list))
