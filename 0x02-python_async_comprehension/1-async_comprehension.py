daf9eaa72a348418fd11dc25e9c5a06047a8e466
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-10 15:43:45 +0100

    new: added 1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""defines the async_comprehension"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """returns a list of float from async_generator"""
+    result  = [i async for i in async_generator()]
+    return (result)
