375bdc20120b99b8a795992c236152229828d59e
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-10 15:27:01 +0100

    refactor: added type annotation for Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """"defines the async_generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """yields a random number"""
     for i in range(10):
         await asyncio.sleep(1)
