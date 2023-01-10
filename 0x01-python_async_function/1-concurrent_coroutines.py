f6edf20f6a8667e305699dff79b76ef0babf0f28
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-10 14:02:34 +0100

    new: added 1-concurrent_coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""defines the wait_n function"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """"an async routine that n wait_random time"""
+    iterables = [wait_random(max_delay) for i in range(n)]
+    n_list = []
+    count = 0
+    for coro in asyncio.as_completed(iterables):
+        earliest = await coro
+        n_list.append(earliest)
+    return n_list
+    
+
+    
+  
