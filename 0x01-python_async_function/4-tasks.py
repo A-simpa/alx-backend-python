41ab4c9b8be5d0eb899e025e7b46a13b8e00e11c
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-15 23:36:59 +0100

    made some changes in 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,10 +8,11 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """"an async routine that n task_wait_random time"""
-    iterables = [task_wait_random(max_delay) for i in range(n)]
+    tasks = [task_wait_random(max_delay) for i in range(n)]
     n_list = []
-    count = 0
-    for coro in asyncio.as_completed(iterables):
-        earliest = await coro
-        n_list.append(earliest)
+
+    for coro in asyncio.as_completed(tasks):
+        n_list.append(await coro)
+
     return n_list
+    
