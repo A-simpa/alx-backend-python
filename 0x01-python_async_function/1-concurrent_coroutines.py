35adbe7282ad410256da457ccdc60ae10c694403
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-15 20:26:28 +0100

    add: added task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+"""defines the coruotine wait_n"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n:int , max_delay):
+    """takes two arguments n, maxdelay, and 
+    calls wait_random n types with max_delay
+    then returns of delays in ascending order."""
+    
+    tasks = []
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+    
+    asc_times =  []
+    for coroutine in asyncio.as_completed(tasks):
+        asc_times.append(await coroutine)
+    
+    return asc_times
+
