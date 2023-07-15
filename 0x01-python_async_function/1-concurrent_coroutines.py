790c6e3481df5e4700c1f5a2b14d0873495f4cc5
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-07-15 20:50:34 +0100

    refactor: annotations added

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,16 +4,16 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int , max_delay: int) -> List:
+async def wait_n(n: int , max_delay: int) -> List[float]:
     """takes two arguments n, maxdelay, and 
     calls wait_random n types with max_delay
     then returns of delays in ascending order."""
     
-    tasks: List = []
+    tasks = []
     for i in range(n):
         tasks.append(wait_random(max_delay))
     
-    asc_times: List =  []
+    asc_times =  []
     for coroutine in asyncio.as_completed(tasks):
         asc_times.append(await coroutine)
     
